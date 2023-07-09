1e92a9851c9b7db28140de70b59f0d3944af1619
Jeffrey <jowoloko@gmail.com>
2023-07-09 13:23:20 +0100

    fix deployment issue

--- src/app/MainApp.jsx
+++ src/app/MainApp.jsx
@@ -1,95 +1,128 @@
-import React, { Component } from 'react';
-import SideBar from '../appParts/sidebar';
-import SideBarBottom from '../appParts/sideBarBottom';
-import { InfoList } from '../info';
-import { getInfoList } from '../info';
-import 'bootstrap/dist/css/bootstrap.css'
-import Feed from '../appParts/feed';
-import Nav from '../nav1';
-import { paginate } from '../utilities/paginate1';
-import reviews from '../app/data';
-import { Route, Redirect, Switch } from 'react-router-dom';
-import Vidly from '../components/vidly';
-import Movie from '../linkComponents/Movie';
-import NotFound from '../linkComponents/NotFound';
-import LoginForm from '../linkComponents/Login';
-import Register from '../linkComponents/Register';
-import homePage from '../appParts/home';
-import sideMenu from '../appParts/sidebar2';
+import React, { Component } from "react";
+import SideBar from "../appParts/sidebar";
+import SideBarBottom from "../appParts/sideBarBottom";
+import { InfoList } from "../info";
+import { getInfoList } from "../info";
+import "bootstrap/dist/css/bootstrap.css";
+import Feed from "../appParts/feed";
+import Nav from "../nav1";
+import { paginate } from "../utilities/paginate1";
+import reviews from "../app/data";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Movie from "../linkComponents/Movie";
+import NotFound from "../linkComponents/NotFound";
+import LoginForm from "../linkComponents/Login";
+import Register from "../linkComponents/Register";
+import homePage from "../appParts/home";
+import sideMenu from "../appParts/sidebar2";
 
 class MainApp extends Component {
-    state = { List: InfoList, onList: getInfoList(), status: "student",
-    data: reviews, pageSize: 3, currentPage: 1  }
+  state = {
+    List: InfoList,
+    onList: getInfoList(),
+    status: "student",
+    data: reviews,
+    pageSize: 3,
+    currentPage: 1,
+  };
 
-    
-    
-    handleSwitchList = listItem => {
-        this.setState({selectedList : listItem})
-        // console.log(listItem.name)
-    }
-    handleSwitch = page => {
-        this.setState({currentPage : page})
-        console.log(page)
-      }
-    
-    render() { 
-        const persons = paginate(this.state.data, this.state.currentPage, this.state.pageSize)
-        
-        return (
-        <div className='app'>
-            <Nav />
-                <div className='app-body'>
-                        
-                        {/* <SideBar items={this.state.onList} 
+  handleSwitchList = (listItem) => {
+    this.setState({ selectedList: listItem });
+    // console.log(listItem.name)
+  };
+  handleSwitch = (page) => {
+    this.setState({ currentPage: page });
+    console.log(page);
+  };
+
+  render() {
+    const persons = paginate(
+      this.state.data,
+      this.state.currentPage,
+      this.state.pageSize
+    );
+
+    return (
+      <div className="app">
+        <Nav />
+        <div className="app-body">
+          {/* <SideBar items={this.state.onList} 
                         status={this.state.status}
                         switchList={this.handleSwitchList} 
                         selectedItem={this.state.selectedList} /> */}
-                        {/* {/* <Feed  items={this.state.onList}/>} */}
-                        <div className='feed'>
-                            <Switch>
-                                <Route path='/homePage' component={homePage} />
-                                
-                                <Route path='/movies/:id'exact component={Movie} />
-                                <Route path='/movies' component={homePage} />
-                                <Route path='/news' render={(props) =>
-                                <SideBarBottom reviews={this.state.data} 
-                                pageSize={this.state.pageSize} 
-                                currentPage={this.state.currentPage}
-                                switchPage={this.handleSwitch} 
-                                persons={persons} {...props} />}
-                            />
-                                <Route path='/register' 
-                                render={(props) => <Register data={this.state.data} {...props} />} />
-                                <Route path='/login' component={LoginForm} />
-                                <Route path='/not-found' component={NotFound} />
-                                <Route path='/home' render={(props) => 
-                                    <Feed  items={this.state.onList}
-                                            status = {this.state.status}
-                                            selectedItem={this.state.selectedList}
-                                            switchList={this.handleSwitch}
-                                            {...props}/>} />
-                                <Route path='/News' render={(props) => 
-                                        <div className='sbBottom'>
-                                            <SideBarBottom reviews={this.state.data} 
-                                            pageSize={this.state.pageSize} 
-                                            currentPage={this.state.currentPage}
-                                            switchPage={this.handleSwitch} 
-                                            persons={persons} {...props}
-                                        />
-                                    </div>} />
-                                <Route exact path='/' render={(props) => 
-                                    <Feed  items={this.state.onList} {...props}/>} />
-                                <Redirect from='/' to='/movies'/>
-                                <Redirect to='/not-found'/>
-                            </Switch>
-                            
-                        </div>
-                </div>
-         </div>
-            
-        );
-    }
+          {/* {/* <Feed  items={this.state.onList}/>} */}
+          <div className="feed">
+            <Switch>
+              <Route path="/homePage" component={homePage} />
+
+              <Route path="/movies/:id" exact component={Movie} />
+              <Route path="/movies" component={homePage} />
+              <Route
+                path="/news"
+                render={(props) => (
+                  <SideBarBottom
+                    reviews={this.state.data}
+                    pageSize={this.state.pageSize}
+                    currentPage={this.state.currentPage}
+                    switchPage={this.handleSwitch}
+                    persons={persons}
+                    {...props}
+                  />
+                )}
+              />
+              <Route
+                path="/register"
+                render={(props) => (
+                  <Register data={this.state.data} {...props} />
+                )}
+              />
+              <Route path="/login" component={LoginForm} />
+              <Route path="/not-found" component={NotFound} />
+              <Route
+                path="/home"
+                render={(props) => (
+                  <Feed
+                    items={this.state.onList}
+                    status={this.state.status}
+                    selectedItem={this.state.selectedList}
+                    switchList={this.handleSwitch}
+                    {...props}
+                  />
+                )}
+              />
+              <Route
+                path="/News"
+                render={(props) => (
+                  <div className="sbBottom">
+                    <SideBarBottom
+                      reviews={this.state.data}
+                      pageSize={this.state.pageSize}
+                      currentPage={this.state.currentPage}
+                      switchPage={this.handleSwitch}
+                      persons={persons}
+                      {...props}
+                    />
+                  </div>
+                )}
+              />
+              <Route
+                exact
+                path="/"
+                render={(props) => (
+                  <Feed items={this.state.onList} {...props} />
+                )}
+              />
+              <Redirect from="/" to="/movies" />
+              <Redirect to="/not-found" />
+            </Switch>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default MainApp;
-{/* <SideBarBottom /> */}
+{
+  /* <SideBarBottom /> */
+}
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
  import { Provider } from 'react-redux';
 import { store } from './app/store';
 import { BrowserRouter } from 'react-router-dom';
-import './webPage.css'
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 // import Header from './Header'
