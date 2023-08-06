9ead3813a4c0827e8bc7f201d2b3f00e5753f7a5
Jeffrey <jowoloko@gmail.com>
2023-08-06 21:12:22 +0100

    add new name

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -56,7 +56,7 @@ function HomePage() {
           <div class="mr-auto place-self-center lg:col-span-7">
             <h1 class="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl text-indigo-700">
               Welcome to <br />
-              NUESA Updates.
+              Axis.
             </h1>
             <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl">
               As we embrace the spirit of continuous growth and development, we
